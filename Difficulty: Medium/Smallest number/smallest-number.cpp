5636db16e089df310063986be6d1d077142a1694
Mohini Gupta <124530250+mohini-sep@users.noreply.github.com>
2024-07-15 22:32:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest number/smallest-number.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string smallestNumber(int s, int d) {
+        // code here
+        string ans="";
+        int i=1;
+        while(i<=d){
+            int num = s-(d-i)*9<=0?0:s-(d-i)*9;
+            if(num==0 and i==1)num=1;
+            if(num>9)return "-1";
+            ans+=(num+'0');
+            i++;
+            s-=num;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int s, d;
+        cin >> s >> d;
+        Solution ob;
+        cout << ob.smallestNumber(s, d) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
