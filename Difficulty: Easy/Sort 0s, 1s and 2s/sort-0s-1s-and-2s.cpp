3da60e20ab386099917a0a9850d530016987bcdd
Mohini Gupta <124530250+mohini-sep@users.noreply.github.com>
2024-12-05 22:26:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -8,7 +8,7 @@ class Solution {
   public:
     void sort012(vector<int>& arr) {
         // code here
-        int zeros=0,ones=0;
+         int zeros=0,ones=0;
         for(int i=0;i<arr.size();i++){
             if(arr[i]==0) zeros++;
             else if(arr[i]==1)ones++;
@@ -53,6 +53,7 @@ int main() {
         }
 
         cout << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
