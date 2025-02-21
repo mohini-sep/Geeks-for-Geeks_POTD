68a3e42e5b9e7ace02b1d9fb8dd8df412e87dc97
Mohini Gupta <124530250+mohini-sep@users.noreply.github.com>
2025-02-21 22:50:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -1,49 +1,43 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-    //Function to check if brackets are balanced or not.
-    bool ispar(string x)
-    {
-        // Your code here
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        // code here
          stack<char>st;
-        for(auto i:x){
-            if(i=='('|| i=='[' || i=='{') st.push(i);
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
             else{
-                 if(st.empty()) return false;
-                if(i==')' && st.top()=='(') st.pop();
-                else if(i==']' && st.top()=='[') st.pop();
-                else if(i=='}' && st.top()=='{') st.pop();
+                if(!st.empty() && ((st.top()=='(' && s[i]==')') || (st.top()=='[' && s[i]==']')||
+                (st.top()=='{' && s[i]=='}'))) st.pop();
                 else return false;
             }
         }
-        if(!st.empty()) return false;
-        return true;
+        return st.empty();
     }
-
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
-   int t;
-   string a;
-   cin>>t;
-   while(t--)
-   {
-       cin>>a;
-       Solution obj;
-       if(obj.ispar(a))
-        cout<<"balanced"<<endl;
-       else
-        cout<<"not balanced"<<endl;
-   }
+int main() {
+    int t;
+    string a;
+    cin >> t;
+    while (t--) {
+        cin >> a;
+        Solution obj;
+        if (obj.isBalanced(a))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+
+        cout << "~"
+             << "\n";
+    }
 }
 // } Driver Code Ends
