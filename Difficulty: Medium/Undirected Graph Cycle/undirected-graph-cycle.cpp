69e02d2ff75230c96cee866a4ac413ba3860a75b
Mohini Gupta <124530250+mohini-sep@users.noreply.github.com>
2025-04-04 23:11:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+
+        bool helper(int start, vector<int>adj[], vector<int>&vis){
+        vis[start]=1;
+        queue<pair<int,int>>q;
+        q.push({-1, start});
+        while(!q.empty()){
+            int parent=q.front().first;
+            int node=q.front().second;
+            q.pop();
+            
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push({node, it});
+                }
+                else if(it!=parent){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
+        vector<int>adj[V];
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+            
+        }
+        
+        vector<int>vis(V,0);
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(helper(i, adj, vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        cin.ignore();
+        vector<vector<int>> edges;
+        for (int i = 1; i <= E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u, v});
+        }
+
+        Solution obj;
+        bool ans = obj.isCycle(V, edges);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
