685daaa4445e4862cc92f1d2ba2ff3d8491683af
Mohini Gupta <124530250+mohini-sep@users.noreply.github.com>
2025-04-05 22:51:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -2,16 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 class Solution {
   public:
-    // Function to find the number of islands.
-    int n,m;
+     int n,m;
     vector<vector<int>> dir{{1,0},{-1,0},{0,-1},{0,1},{1,1},{-1,-1},{1,-1},{-1,1}};
     void solve(vector<vector<char>>& grid,int i,int j){
         //base case
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0') return;
-        grid[i][j]='0';
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='W') return;
+        grid[i][j]='W';
         for(auto &it:dir){
             int newi=i+it[0];
             int newj=j+it[1];
@@ -19,23 +20,23 @@ class Solution {
         }
         
     }
-    int numIslands(vector<vector<char>>& grid) {
-        // Code here
+    int countIslands(vector<vector<char>>& grid) {
+    
         n=grid.size();
         m=grid[0].size();
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
+                if(grid[i][j]=='L'){
                     solve(grid,i,j);
                     count++;
                 }
             }
         }
         return count;
-        
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
@@ -51,8 +52,11 @@ int main() {
             }
         }
         Solution obj;
-        int ans = obj.numIslands(grid);
+        int ans = obj.countIslands(grid);
         cout << ans << '\n';
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
